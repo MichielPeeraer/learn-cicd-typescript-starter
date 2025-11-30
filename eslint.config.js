c6a273be5cb542541ad4a626e0b6379b6837e047
MichielPeeraer <peeraer.michiel@gmail.com>
2025-11-30 22:14:58 +0100

    Added security check

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import pluginSecurity from "eslint-plugin-security";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
@@ -11,5 +12,6 @@ export default defineConfig([
     languageOptions: { globals: globals.node }
   },
   tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
   globalIgnores(["dist/"])
 ]);
